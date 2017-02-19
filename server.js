38d27b58c4fa4f0a7471a35bdec47201131bebb7
Sanjee <sanjee.yogeswaran@gmail.com>
2017-02-19 07:24:57 -0600

    tet

--- server.js
+++ server.js
@@ -1,34 +1,25 @@
-/*eslint no-console:0 */
 'use strict';
-require('core-js/fn/object/assign');
-const webpack = require('webpack');
-const WebpackDevServer = require('webpack-dev-server');
-const config = require('./webpack.config');
-const open = require('open');
-/**
- * Flag indicating whether webpack compiled for the first time.
- * @type {boolean}
- */
-let isInitialCompilation = true;
 
-const compiler = webpack(config);
-new WebpackDevServer(compiler, config.devServer)
-.listen(config.port, 'localhost', (err) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log('Listening at localhost:' + config.port);
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Setup view engine
+app.set('view engine', 'pug');
+
+app.use(express.static(path.resolve(path.join(__dirname, '/dist'))));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+app.get('/latestgpo', (req, res) => {
+  res.render('index');
 });
 
-compiler.plugin('done', () => {
-  if (isInitialCompilation) {
-    // Ensures that we log after webpack printed its stats (is there a better way?)
-    setTimeout(() => {
-      console.log('\n✓ The bundle is now ready for serving!\n');
-      console.log('  Open in iframe mode:\t\x1b[33m%s\x1b[0m',  'http://localhost:' + config.port + '/webpack-dev-server/');
-      console.log('  Open in inline mode:\t\x1b[33m%s\x1b[0m', 'http://localhost:' + config.port + '/\n');
-      console.log('  \x1b[33mHMR is active\x1b[0m. The bundle will automatically rebuild and live-update on changes.')
-    }, 350);
-  }
-  isInitialCompilation = false;
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
 });
+
